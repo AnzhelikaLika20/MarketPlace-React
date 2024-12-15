8027e930c1efc13fe570b14668b589086a24ddca
Anzhelika Gakal <gakal.a@mail.ru>
2024-12-15 23:49:36 +0300

    remove css 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import ProductList from './components/ProductList/ProductList';
 import Sidebar from './components/SideBar/SideBar';
 import NavigationBar from './components/NavigationBar/NavigationBar';
-import './styles/global.css';
 import { products } from "./data/products.ts";
 
 const App: React.FC = () => {
